a8a379a50ff72b26e9f7f0d09ed06799099b894f
Michael Steinacher <mstein921@googlemail.com>
2016-04-08 22:36:43 +0200

    Initial

---
+++ src/main/java/org/storetest/CacheAccess.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (C) Waldemar Winckel GmbH & Co KG - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * See http://www.winckel.de for further information.
+ */
+package org.storetest;
+
+import org.infinispan.Cache;
+import org.infinispan.manager.CacheContainer;
+
+import javax.annotation.Resource;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Path("test")
+public class CacheAccess {
+
+    private final static Logger logger = Logger.getLogger(CacheAccess.class.getName());
+
+    @Resource(lookup = "java:jboss/infinispan/container/teststore")
+    protected CacheContainer container;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response test() {
+
+        Response.ResponseBuilder builder = Response.ok();
+
+        try {
+            Cache<String, Long> cache = container.getCache("test");
+            Long incValue = cache.getOrDefault("value", new Long(0));
+
+            logger.info(String.format("Value: %d", incValue));
+            builder.entity(String.format("Value: %d", incValue));
+
+            cache.put("value", new Long(incValue.longValue() + 1));
+        }
+        catch (Exception ex) {
+            logger.log(Level.SEVERE, "Test failed ;-)", ex);
+        }
+
+        return builder.build();
+    }
+}
---
+++ src/main/java/org/storetest/JaxApplication.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright (C) Waldemar Winckel GmbH & Co KG - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * See http://www.winckel.de for further information.
+ */
+package org.storetest;
+
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class JaxApplication extends Application {
+}
