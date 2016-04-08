9c1944410adf3bb85c2db14dec8997efb0b6b50e
Michael Steinacher <mstein921@googlemail.com>
2016-04-08 22:38:33 +0200

    Remove wrong copyright

--- src/main/java/org/storetest/CacheAccess.java
+++ src/main/java/org/storetest/CacheAccess.java
@@ -1,8 +1,3 @@
-/**
- * Copyright (C) Waldemar Winckel GmbH & Co KG - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * See http://www.winckel.de for further information.
- */
 package org.storetest;
 
 import org.infinispan.Cache;
